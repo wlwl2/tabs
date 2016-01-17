fa2c47ce9595104374e3862a567c293b0c406d4a
wlwl2 <manvinedi@gmail.com>
2016-01-17 22:08:11 +0800

    first commit

---
+++ js/script.js
@@ -0,0 +1,35 @@
+var juis = {};
+
+juis.tabList = {
+  tabs: document.querySelectorAll(".g-tab"),
+  init: function() {
+    //if the tab list doesn't exist
+    if (!document.querySelector(".g-tablist")) {
+      return;
+    }
+    juis.tabList.setup();
+    juis.tabList.bind();
+  },
+  setup: function () {
+    //show tabs
+    document.querySelector(".g-tablist").setAttribute("aria-hidden", "false");
+
+    //check if any tabs are highlighted
+    //if not, default to first
+    //hide non-selected tabs
+
+  },
+  bind: function() {
+    for (var i=0; i++; i < juis.tabList.tabs.length) {
+      juis.tabList.tabs.addEventListener("click", juis.tabList.activateTab);
+    }
+  },
+  activateTab: function () {
+    //switch to the selected tab
+
+  }
+};
+
+document.addEventListener("DOMContentLoaded", function() {
+  juis.tabList.init();
+});
