60b449eadc9ab785fa2a8a25208ef7a15688e72a
William Lim <manvinedi@gmail.com>
2016-01-18 10:42:53 +0800

    update

--- js/script.js
+++ js/script.js
@@ -11,6 +11,7 @@ juis.tabList = {
     }
     juis.tabList.setup();
     juis.tabList.bind();
+    juis.tabList.activateTab();
   },
   setup: function () {
     //show tabs
@@ -19,6 +20,17 @@ juis.tabList = {
     //check if any tabs are highlighted
     //if not, default to first
     //hide non-selected tabs
+    if (localStorage.length > 0) {
+      var tabbuttonid = localStorage.getItem("selectedtab").substr(localStorage.getItem("selectedtab").length - 1)
+      document.getElementById("tab"+tabbuttonid).setAttribute("aria-hidden", "false");
+    }
+    else {
+      document.getElementById("tab1").setAttribute("aria-hidden", "false");
+      document.getElementById("tab2").setAttribute("aria-hidden", "true");
+      document.getElementById("tab3").setAttribute("aria-hidden", "true");
+      localStorage.setItem('selectedtab', 'tabbutton1')
+    }
+
 
   },
   bind: function() {
@@ -28,7 +40,26 @@ juis.tabList = {
   },
   activateTab: function () {
     //switch to the selected tab
+    document.getElementById("tabbutton1").onclick = function(){
+      document.getElementById("tab1").setAttribute("aria-hidden", "false");
+      document.getElementById("tab2").setAttribute("aria-hidden", "true");
+      document.getElementById("tab3").setAttribute("aria-hidden", "true");
+      localStorage.setItem('selectedtab', 'tabbutton1')
+    }
 
+    document.getElementById("tabbutton2").onclick = function(){
+      document.getElementById("tab2").setAttribute("aria-hidden", "false");
+      document.getElementById("tab1").setAttribute("aria-hidden", "true");
+      document.getElementById("tab3").setAttribute("aria-hidden", "true");
+      localStorage.setItem('selectedtab', 'tabbutton2')
+    }
+
+    document.getElementById("tabbutton3").onclick = function(){
+      document.getElementById("tab3").setAttribute("aria-hidden", "false");
+      document.getElementById("tab2").setAttribute("aria-hidden", "true");
+      document.getElementById("tab1").setAttribute("aria-hidden", "true");
+      localStorage.setItem('selectedtab', 'tabbutton3')
+    }
   }
 };
 
